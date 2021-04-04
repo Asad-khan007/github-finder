1ba7639aaff379a7b607755176c76724b5cd48a7
Asad-khan007 <69400466+Asad-khan007@users.noreply.github.com>
2021-04-04 15:06:03 -0700

    created Search Bar
    added the search bar component with functionality for the Github finder applicaiton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './component/layout/Navbar';
 import Users from './component/users/User';
 import './App.css';
 import axios from 'axios';
+import Search from './component/users/search';
 
 class App extends Component{
 
@@ -14,17 +15,25 @@ class App extends Component{
 
 async componentDidMount() {
   this.setState( {loading: true} )
-  const res = await axios.get('https://api.github.com/users')
+  const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
   this.setState( {loading:false , users:res.data} )
+  console.log(res.data.items)
+  }
+
+  searchUsers = async (text) => {
+    this.setState( {loading: true} )
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState( {  users: res.data.items, loading:false} )
   }
  
   render() {
-    console.log(this.state.users)
+
     return (
      <div>
         <Navbar title='Github Finder' />
         
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
      </div>
